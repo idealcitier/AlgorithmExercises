f2bc295108093b16e2338d5a6a2822a419027e67
idealcitier <leeguo@126.com>
2018-09-14 14:19:57 +0800

    change the ATOI.java

--- src/03/ATOI.java
+++ src/03/ATOI.java
@@ -8,13 +8,37 @@ public class ATOI{
 	
 	}
 	public static int strToInt(String str){
+		if(str == null || str.equals(""))
+			return 0;
+		str = str.trim();
+		int i = 0;
+		char flag = '+';
 		char[] ch = str.toCharArray();
+		if(ch[0] == '-'){
+			flag = '-';
+			i++;
+		}else if(ch[i] == '+'){
+			flag = '+';
+			i++;
+		}
 		int num=0;
-		for(int i=0;i < ch.length;i++){
+		for(;i < ch.length && ch[i] <= '9' && ch[i] >= '0';i++){
 			int tmp = ch[i] - '0';
 			num = num * 10 + tmp;
 		}
-		return num;	
+		if(flag == '-'){
+			num = -num;
+		}
+		if(num > Integer.MAX_VALUE){
+			return Integer.MAX_VALUE;
+		}
+		else if(num < Integer.MIN_VALUE){
+			return Integer.MIN_VALUE;
+		}else{
+			return num;
+		}
+
+		
 	
 	}
 }
