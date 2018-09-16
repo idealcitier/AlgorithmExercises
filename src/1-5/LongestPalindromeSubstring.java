95cca9445b1c9c0e06d5d4c7a37f1f5d5d6b1b33
idealcitier <leeguo@126.com>
2018-09-16 16:43:21 +0800

    add longestPalindromeSubstring

--- src/1-5/LongestPalindromeSubstring.java
+++ src/1-5/LongestPalindromeSubstring.java
@@ -1,6 +1,30 @@
+import java.util.*;
 public class LongestPalindromeSubstring{
 	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		Set<String> set = new HashSet<>();
+		set = getSubstring(str);
+		Iterator it = set.iterator();
+		int len = 0;
+		String longestPalindromeSubstring = "";
+		while(it.hasNext()){
+			String  substring = (String)it.next();
+			//System.out.println(substring);
+			if(isPalindromic(substring)){
+				if(substring.length() > len){
+					len = substring.length();
+					longestPalindromeSubstring = substring;
 	
+				}else{
+					continue;
+				}
+			}
+
+		}
+
+		System.out.println("longestPalindromeSubstring= " + longestPalindromeSubstring );
+		System.out.println("len = " + len);
 	}
 
 	public static boolean isPalindromic(String str){
@@ -14,8 +38,14 @@ public class LongestPalindromeSubstring{
 		}
 	}
 
-	public static String[] getSubstring(String str){
+	public static Set<String> getSubstring(String str){
+		Set<String> set = new HashSet<>();
+		for(int i = 0;i<str.length();i++){
+			for (int j = i+1;j < str.length()+1;j++)
+				set.add(str.substring(i,j));
+		}
 		
+		return set;
 	}
 
 
