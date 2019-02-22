5e1be5d28236cd99c5f63d7c45d05d54ef3675fd
idealcitier <leeguo@126.com>
2019-02-22 20:35:42 +0800

    update linkedListCycle

--- leetCode/linkedListCycle/Solution.java
+++ leetCode/linkedListCycle/Solution.java
@@ -14,11 +14,12 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
         if(head == null) {return false;}
-        ListNode fast, slow = head;
+        ListNode fast = head;
+        ListNode slow = head;
         while(fast != null && slow != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
-            if(slow == next){
+            if(slow == fast){
                 return true;
             }
         }
